09032ccecd929a9774c9a424484ca0729acb7ee3
mostsfa538 <mostafa51moktar@gmail.com>
2024-09-02 23:20:57 +0300

    Define which routes

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,15 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ returns False - path and excluded_paths will be used later """
-        return False
+        if path is None or excluded_paths is None:
+            return True
+        for excluded_path in excluded_paths:
+            if excluded_path[-1] == '*' and path.startswith(
+                    excluded_path[:-1]):
+                return False
+            elif path == excluded_path or path + '/' == excluded_path:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ returns None - request will be the Flask request object """
@@ -18,4 +26,3 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """ returns None - request will be the Flask request object """
         return None
-
