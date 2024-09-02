ee522a8879adfb08115576130c4934967f10efa2
mostsfa538 <mostafa51moktar@gmail.com>
2024-09-02 22:25:53 +0300

    Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/api/v1/unauthorized', methods=['GET'])
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized_access():
     """ Route to trigger 401 Unauthorized error """
     abort(401)
