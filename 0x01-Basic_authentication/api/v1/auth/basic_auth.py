7b48c6ffe0a2648b9759066d6bbe99afa77bef0a
mostsfa538 <mostafa51moktar@gmail.com>
2024-09-03 15:03:27 +0300

    Basic - User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -30,3 +30,18 @@ class BasicAuth(Auth):
             return val.decode('UTF-8')
         except Exception:
             return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str
+                                 ) -> (str, str):
+        """returns the user email and password from the Base64 decoded value"""
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if not isinstance(decoded_base64_authorization_header, str):
+            return (None, None)
+        if ':' not in decoded_base64_authorization_header:
+            return (None, None)
+        data = decoded_base64_authorization_header.split(':')
+        email = data[0]
+        password = ':'.join(data[1:])
+        return (email, password)
