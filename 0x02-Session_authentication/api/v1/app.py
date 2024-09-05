36a6399d7b1f973fec3f6e8ab9048bdd0d6d94fe
mostsfa538 <mostafa51moktar@gmail.com>
2024-09-05 21:18:18 +0300

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -35,7 +35,8 @@ def before_request():
         return
     if not auth.require_auth(request.path, ['/api/v1/status/',
                                             '/api/v1/unauthorized/',
-                                            '/api/v1/forbidden/']):
+                                            '/api/v1/forbidden/',
+                                            '/api/v1/auth_session/login/']):
         return
     if auth.authorization_header(request) is None:
         abort(401)
