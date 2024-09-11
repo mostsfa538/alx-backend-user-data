bce8e056fc4fa9f672233cd497e2a999e5883ee6
mostsfa538 <mostafa51moktar@gmail.com>
2024-09-11 06:30:43 +0300

    Credentials validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -24,3 +24,11 @@ class Auth:
         except Exception:
             return self._db.add_user(email, _hash_password(password))
         raise ValueError(f"User {email} already exists")
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """ check the password with bcrypt.checkpw """
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('UTF-8'), user.hashed_password)
+        except Exception:
+            return False
--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -2,7 +2,16 @@
 """
 Main file
 """
-from auth import _hash_password
+from auth import Auth
 
-print(_hash_password("Hello Holberton"))
+email = 'bob@bob.com'
+password = 'MyPwdOfBob'
+auth = Auth()
 
+auth.register_user(email, password)
+
+print(auth.valid_login(email, password))
+
+print(auth.valid_login(email, "WrongPwd"))
+
+print(auth.valid_login("unknown@email", password))
