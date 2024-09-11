d11088cefd0d78f2e91045f6c5f85a93ef757675
mostsfa538 <mostafa51moktar@gmail.com>
2024-09-11 06:53:50 +0300

    Log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -26,15 +26,18 @@ def users():
         return jsonify({"message": "email already registered"}), 404
 
 
-@app.route('/sessions', methods=['POST'])
+@app.route("/sessions", methods=["POST"])
 def login():
-    """ create a new session for the user """
-    email = request.form.get('email')
-    password = request.form.get('password')
-    if AUTH.valid_login(email, password):
-        abort(401)
+    """POST /sessions
+    generate a session_id for a user when he logs in
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
 
+    if not AUTH.valid_login(email, password):
+        abort(401)
     session_id = AUTH.create_session(email)
+
     out = jsonify(email=email, message="logged in")
     out.set_cookie("session_id", session_id)
     return out
